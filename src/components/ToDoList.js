6ceec4a7bd06d9d2c32adecdce11f89323840a48
Fatma Elasouad <fxe920@cs.bham.ac.uk>
2020-10-22 16:00:12 +0100

    Basic React TodoList

---
+++ src/components/ToDoList.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import components
+import Todo from "./Todo";
+
+const ToDoList = ({ todos, setTodos, filteredTodos }) => {
+  return (
+    <div className="todo-container">
+      <ul className="todo-list">
+        {filteredTodos.map((todo) => (
+          <Todo
+            text={todo.text}
+            key={todo.id}
+            setTodos={setTodos}
+            todo={todo}
+            todos={todos}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDoList;
